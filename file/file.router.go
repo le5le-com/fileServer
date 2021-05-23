2ec9721b11b5107fb46052591e29d0bc18e3cd32
Alsmile <alsmile123@qq.com>
2021-05-23 18:15:27 +0800

    router

--- file/file.router.go
+++ file/file.router.go
@@ -10,11 +10,19 @@ import (
 func Route(route *iris.Application) {
 
 	route.Get("/file/{path:path}", File)
+	route.Post("/file", middlewares.Auth, Limit, UploadFile)
+	route.Patch("/file/{path:path}", middlewares.Auth, FileAttr)
+	route.Delete("/file/{path:path}", middlewares.Auth, DelFile)
+
 	route.Post("/api/file", middlewares.Auth, Limit, UploadFile)
 	route.Patch("/api/file/{path:path}", middlewares.Auth, FileAttr)
 	route.Delete("/api/file/{path:path}", middlewares.Auth, DelFile)
 
 	route.Get("/image/{path:path}", Image)
+	route.Post("/image", middlewares.Auth, Limit, UploadImage)
+	route.Patch("/image/{path:path}", middlewares.Auth, FileAttr)
+	route.Delete("/image/{path:path}", middlewares.Auth, DelFile)
+
 	route.Post("/api/image", middlewares.Auth, Limit, UploadImage)
 	route.Patch("/api/image/{path:path}", middlewares.Auth, FileAttr)
 	route.Delete("/api/image/{path:path}", middlewares.Auth, DelFile)
